1eeb6d4332a95e13f85ec80e9824224768473c7c
Skyler <sky.i.frey@gmail.com>
2025-07-18 14:08:01 -0400

    Initial commit

---
+++ src/main/java/net/shardskye/shardsbiomemod/ShardsBiomeModClient.java
@@ -0,0 +1,10 @@
+package net.shardskye.shardsbiomemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShardsBiomeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/shardskye/shardsbiomemod/ShardsBiomeModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.shardskye.shardsbiomemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ShardsBiomeModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
